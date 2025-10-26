c4c9507d47cc6cd35fbf5bb463a9a6375ff14a9e
saltyjoss <joss.salton@gmail.com>
2025-10-26 22:54:28 +0000

    feat(OpenGL Sim): Added basic test window, intiialisation check for the new libs included.

--- SimulatorOpenGL/Main.cpp
+++ SimulatorOpenGL/Main.cpp
@@ -3,5 +3,46 @@
 #include <GLFW/glfw3.h>
 
 int main() {
+	// Initialize GLFW
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Simulator", nullptr, nullptr);	// Create window (windowed mode) and its OpenGL context
+
+	if (!window) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);	// Make the window's context current
+
+	gladLoadGL(); // Load OpenGL functions
+
+	glViewport(0, 0, 1920, 1080);	// Set the viewport size
+
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);	// Set clear color
+
+
+	// Load OpenGL functions using GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Main loop
+	while (!glfwWindowShouldClose(window)) {
+		
+		glClear(GL_COLOR_BUFFER_BIT);	// Render here
+		
+		glfwSwapBuffers(window);	// Swap front and back buffers
+		glfwPollEvents();			// Poll for and process events
+	}
+
+	// Clean up and exit
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
