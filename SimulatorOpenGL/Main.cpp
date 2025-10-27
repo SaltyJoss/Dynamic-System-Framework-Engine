aecd9ed601e947f1a28bf9b821e0f112b9acc22e
saltyjoss <joss.salton@gmail.com>
2025-10-27 16:13:32 +0000

    fixes

--- SimulatorOpenGL/Main.cpp
+++ SimulatorOpenGL/Main.cpp
@@ -3,84 +3,19 @@
 #include <GLFW/glfw3.h>
 #include "imgui.h"
 #include "GUIManager.h"
+#include "Application.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
-// Resize callback
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
-
+// Main method fpr 
 int main() {
-	// Initialize GLFW
-	if (!glfwInit()) {
-		std::cerr << "Failed to initialize GLFW" << std::endl;
+	Application app;
+
+	if (!app.Initialise())
 		return -1;
-	}
 
-	// Window hints
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-	glfwWindowHint(GLFW_DEPTH_BITS, 24);
-
-	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Simulator V0.1", nullptr, nullptr);	// Create window (windowed mode) and its OpenGL context
-
-	// Create Window
-	if (!window) {
-		std::cerr << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	// Load OpenGL functions using GLAD
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cerr << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-
-	glEnable(GL_DEPTH_TEST);
-	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);	// Set clear color
-
-	// ImGui Initialisation
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	(void)io;
-
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
-
-	// Initialise GUI Manager
-	GUIManager gui;
-
-	// Main loop
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		gui.BeginFrame();
-		gui.DrawPanel();
-		gui.EndFrame();
-
-		glfwSwapBuffers(window);	// Swap front and back buffers
-	}
-
-	// Clean up and exit
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-	glfwDestroyWindow(window);
-	glfwTerminate();
+	app.Run();
+	app.Shutdown();
 	
 	return 0;
 }
