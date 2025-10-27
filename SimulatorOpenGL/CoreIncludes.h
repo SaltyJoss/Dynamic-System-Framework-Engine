c9643912ec7a1f8c2383bf1f6f085974a0fc12b9
saltyjoss <joss.salton@gmail.com>
2025-10-27 18:18:23 +0000

    feat(Simulator): Addition of ControlPanel header and cpp file

---
+++ SimulatorOpenGL/CoreIncludes.h
@@ -0,0 +1,23 @@
+#ifndef CORE_INCLUDES_H
+#define CORE_INCLUDES_H
+
+// Standard Library
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+// Math Logic Library
+// THIS IS GOING INCLUDE MY MATHEMATICAL LOGIC
+
+// Graphics
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// ImGui
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#endif
+
