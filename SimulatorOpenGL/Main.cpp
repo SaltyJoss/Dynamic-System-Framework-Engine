7a1c558c5ccf096ad213684bbaf87324f474abe9
saltyjoss <joss.salton@gmail.com>
2025-10-27 11:20:17 +0000

    feat(OpenGL Sim): Added basic Shader for current triangle object using interpolation
    - Changed: VAO1.LinkVBO to VAO1.LinkAttrib
    - LinkAttrib ++ 3 new arguments
       - GLuint numComponents
       - GLenum type
       - GLsizeiptr stride
       - void* offset

--- SimulatorOpenGL/Main.cpp
+++ SimulatorOpenGL/Main.cpp
@@ -8,13 +8,14 @@
 #include "EBO.h"
 
 // Vertices of a triangle
-GLfloat vertices[] = {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,		// Bottom left Corner {0}
-	 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,		// Bottom right Corner {1}
-	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,	// Top Corner {2}
-	 -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	// Inner left {3}
-	 0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,		// Inner right {4}
-	 0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f			// Inner bottom {5}
+GLfloat vertices[] = 
+{ //				COORDINATES					  /		COLOURS			//
+	-0.5f, -0.5f * float(sqrt(3)) / 3,		0.0f,	0.8f, 0.3f,  0.02f,	// Bottom left Corner {0}
+	 0.5f, -0.5f * float(sqrt(3)) / 3,		0.0f,	0.8f, 0.3f,  0.02f,	// Bottom right Corner {1}
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3,	0.0f,	1.0f, 0.6f,  0.32f,	// Top Corner {2}
+	 -0.5f / 2, 0.5f * float(sqrt(3)) / 6,	0.0f,	0.9f, 0.45f, 0.17f,	// Inner left {3}
+	 0.5f / 2, 0.5f * float(sqrt(3)) / 6,	0.0f,	0.9f, 0.45f, 0.17f,	// Inner right {4}
+	 0.0f, -0.5f * float(sqrt(3)) / 3,		0.0f,	0.8f, 0.3f,  0.02f	// Inner bottom {5}
 
 };
 
@@ -36,7 +37,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);	// Set OpenGL version to 3.4
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	// Use core profile
 
-	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Simulator", nullptr, nullptr);	// Create window (windowed mode) and its OpenGL context
+	GLFWwindow* window = glfwCreateWindow(1080, 1080, "Simulator V0.1", nullptr, nullptr);	// Create window (windowed mode) and its OpenGL context
 
 	if (!window) {
 		std::cerr << "Failed to create GLFW window" << std::endl;
@@ -47,9 +48,9 @@ int main() {
 	glfwMakeContextCurrent(window);	// Make the window's context current
 	gladLoadGL();					// Load OpenGL functions
 
-	glViewport(0, 0, 1920, 1080);			// Set the viewport size
+	glViewport(0, 0, 1080, 1080);			// Set the viewport size
 
-	Shader shaderProgram("default.vert", "default.frag"); // Create shader program from shaders
+	Shader shaderProgram("default.vert", "default.frag"); // Create shader program from shaders	
 	
 	VAO VAO1;		// Create Vertex Array Object
 	VAO1.Bind();	// Bind the VAO
@@ -57,13 +58,15 @@ int main() {
 	VBO VBO1(vertices, sizeof(vertices));	// Create Vertex Buffer Object and link it to vertices
 	EBO EBO1(indices, sizeof(indices));		// Create Element Buffer Object and link it to indices
 
-	VAO1.LinkVBO(VBO1, 0);	// Link VBO to VAO
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);	// Links VAO to VBO and the attributes
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
 	VAO1.Unbind();			// Unbind the VAO
 	VBO1.Unbind();			// Unbind the VBO
 	EBO1.Unbind();			// Unbind the EBO
 
 
-	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);	// Set clear color
+	glClearColor(0.075f, 0.125f, 0.15f, 1.0f);	// Set clear color
 	glClear(GL_COLOR_BUFFER_BIT);			// Clear the color buffer
 	glfwSwapBuffers(window);				// Swap front and back buffers
 
