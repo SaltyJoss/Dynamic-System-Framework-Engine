3aa9a0fdf10b50a562a3f30ba410d6e11ef918ba
saltyjoss <joss.salton@gmail.com>
2025-10-28 09:46:46 +0000

    feat(SimulationPanels): Initialisation of simulation panels in header and cpp files

--- SimulatorOpenGL/SimulationPanels.h
+++ SimulatorOpenGL/SimulationPanels.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef SIMULATION_PANELS_H
+#define SIMULATION_PANELS_H
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+class SimulationPanels 
+{
+public:
+	void Render();
+
+private:
+	void Container(ImGuiWindowFlags panelFlags);
+	void MainPanel(ImGuiWindowFlags panelFlags);
+	void Panel1(ImGuiWindowFlags panelFlags);
+	void Panel2(ImGuiWindowFlags panelFlags);
+	void Panel3(ImGuiWindowFlags panelFlags);
+	void Panel4(ImGuiWindowFlags panelFlags);
+};
+
+
+#endif 
