c243baffd4fc49f8a3213bc63030d7287b3fbbb5
saltyjoss <joss.salton@gmail.com>
2025-10-29 13:58:49 +0000

    feat(Simulation Manager): Initialised basic structure of Simulation Manager Files.

---
+++ SimulatorOpenGL/SimulationManager.cpp
@@ -0,0 +1,17 @@
+#include "SimulationManager.h"
+
+bool SimulationManager::Init() {
+
+}
+
+void SimulationManager::Update(float dt) {
+
+}
+
+void SimulationManager::Render() {
+
+}
+
+void SimulationManager::Terminate() {
+
+}
