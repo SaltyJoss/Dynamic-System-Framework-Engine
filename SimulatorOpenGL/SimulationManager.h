9c7bdcee4d13e4413da75036e6b35a8b674b6bce
saltyjoss <joss.salton@gmail.com>
2025-10-29 13:58:49 +0000

    feat(Simulation Manager): Initialised basic structure of Simulation Manager Files.

--- SimulatorOpenGL/SimulationManager.h
+++ SimulatorOpenGL/SimulationManager.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef SIMULATION_MANAGER_H
+#define SIMULATOR_MANAGER_H
+
+#include "CoreIncludes.h"
+
+class SimulationManager
+{
+public:
+    bool Init();            // create shaders, buffers, etc.
+    void Update(float dt);  // update logic or physics
+    void Render();          // issue OpenGL draw calls
+    void Terminate();       // release GPU resources
+
+private:
+    unsigned int shaderProgram = 0;     // Can use previous logic
+    unsigned int VAO = 0, VBO = 0;      // Can use previous logic
+
+    // optional: camera, time, scene data
+    glm::mat4 view;
+    glm::mat4 projection;
+    glm::mat4 model;
+};
+
+#endif
