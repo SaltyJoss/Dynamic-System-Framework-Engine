1ef90078fafbb61060f4b97e7185cb7d3cdf03f5
saltyjoss <joss.salton@gmail.com>
2025-10-30 18:36:16 +0000

    fixes

--- SimulatorOpenGL/Source/Window/Window.h
+++ SimulatorOpenGL/Source/Window/Window.h
@@ -6,11 +6,14 @@ class IWindow {
 public:
 	virtual void* getNativeWin() = 0;
 	virtual void setNativeWin(void* window) = 0;
-	virtual void onResize(int w, int h) = 0;
+
+	virtual void onKey(int  key, int scancode, int action, int mods) = 0;
+	virtual void onScroll(double delta) = 0;
+	virtual void onResize(int width, int height) = 0;
 	virtual void onClose() = 0;
 
-	int w;
-	int h;
+	int width;
+	int height;
 	std::string header;
 };
 }
