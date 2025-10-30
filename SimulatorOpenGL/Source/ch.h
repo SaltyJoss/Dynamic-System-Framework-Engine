73271a6817907a3b6d6ceb59b41a9b1deaec1c2a
saltyjoss <joss.salton@gmail.com>
2025-10-30 17:01:21 +0000

    Added GLEW library into includes

--- SimulatorOpenGL/Source/ch.h
+++ SimulatorOpenGL/Source/ch.h
@@ -11,6 +11,7 @@
 #include <memory>
 
 // GL includes
+#include <GL/glew.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -19,9 +20,4 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/transform.hpp>
 
-// ImGui
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-
-#define BIND_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+#define GLM_ENABLE_EXPERIMENTAL
