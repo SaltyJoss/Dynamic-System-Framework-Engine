224947e3da58e752b44884f7d14376583103c005
saltyjoss <joss.salton@gmail.com>
2025-10-30 18:35:29 +0000

    feat(OpenGLContext): Add logic to OpenGLContext.cpp

--- SimulatorOpenGL/Source/Render/OpenGLContext.cpp
+++ SimulatorOpenGL/Source/Render/OpenGLContext.cpp
@@ -1,21 +1,65 @@
+#include "ch.h"
 #include "OpenGLContext.h"
 
 namespace render {
-	bool render::OpenGLContext::init(window::IWindow* win)
-	{
-		return false;
+	static void onKey_Callback(GLFWwindow* win, int  key, int scancode, int action, int mods) {
+		auto currentWindow = static_cast<window::IWindow*>(glfwGetWindowUserPointer(win));
+		currentWindow->onKey(key, scancode, action, mods);
 	}
 
-	void render::OpenGLContext::preRender()
-	{
+	static void onScroll_Callback(GLFWwindow* win, double xoffset, double yoffset) {
+		auto currentWindow = static_cast<window::IWindow*>(glfwGetWindowUserPointer(win));
+		currentWindow->onScroll(yoffset);
 	}
 
-	void render::OpenGLContext::postRender()
-	{
+	static void onResize_Callback(GLFWwindow* win, int width, int height) {
+		auto currentWindow = static_cast<window::IWindow*>(glfwGetWindowUserPointer(win));
+		currentWindow->onResize(width, height);
 	}
 
-	void render::OpenGLContext::end()
-	{
+	static void onClose_Callback(GLFWwindow* win) {
+		window::IWindow* currentWindow = static_cast<window::IWindow*>(glfwGetWindowUserPointer(win));
+		currentWindow->onClose();
+	}
+
+	bool render::OpenGLContext::init(window::IWindow* window) {
+		__super::init(window);
+
+		if (!glfwInit()) { std::cerr << "[ERROR]: " << "Failed to initialize GLFW" << std::endl; return false; }
+
+		auto glWindow = glfwCreateWindow(window->width, window->height, window->header.c_str(), nullptr, nullptr);
+		window->setNativeWin(glWindow);
+
+		if (!glWindow) { std::cerr << "[ERROR]: " << "Failed to create GLFW window" << std::endl; return false; }
+
+		glfwSetWindowUserPointer(glWindow, window);
+		glfwSetKeyCallback(glWindow, onKey_Callback);
+		glfwSetScrollCallback(glWindow, onScroll_Callback);
+		glfwSetWindowSizeCallback(glWindow, onResize_Callback);
+		glfwSetWindowCloseCallback(glWindow, onClose_Callback);
+		glfwMakeContextCurrent(glWindow);
+
+		GLenum err = glewInit();
+		if (GLEW_OK != err) { std::cerr << "[ERROR]: " << stderr << "\n" << glewGetErrorString(err) << std::endl; return false; }
+
+		glEnable(GL_DEPTH_TEST);
+
+		return true;
+	}
+
+	void render::OpenGLContext::preRender() {
+		glViewport(0, 0, _window->width, _window->height);
+		glClearColor(0.33f, 0.33f, 0.33f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
+	void render::OpenGLContext::postRender() {
+		glfwPollEvents();
+		glfwSwapBuffers((GLFWwindow*)_window->getNativeWin());
+	}
+
+	void render::OpenGLContext::end() {
+		glfwDestroyWindow((GLFWwindow*)_window->getNativeWin());
+		glfwTerminate();
 	}
 }
-
