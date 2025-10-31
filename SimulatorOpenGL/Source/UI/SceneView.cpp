ffbb4402858a3a330bc9a48374f6544c6f9e781e
saltyjoss <joss.salton@gmail.com>
2025-10-31 11:15:30 +0000

    feat(Scene View): Add scene view logic, will change to adapt my old logic, this is BAREBONES

--- SimulatorOpenGL/Source/UI/SceneView.cpp
+++ SimulatorOpenGL/Source/UI/SceneView.cpp
@@ -1,1 +1,44 @@
-#include "UI/SceneView.h"
+#include "ch.h"
+
+#include "SceneView.h"
+#include "imgui.h"
+
+namespace gui{
+	void SceneView::resize(int32_t width, int32_t height) {
+		_size.x = width;
+		_size.y = height;
+	}
+
+	void SceneView::onMouseMove(double x, double y, elements::eInputButton button) { _camera->onMouseMove(x, y, button); }
+	void SceneView::onMouseWheel(double delta) { _camera->onMouseWheel(delta); }
+
+	void SceneView::loadMesh(const std::string& filepath) {
+		if (!_mesh) { _mesh = std::make_shared < elements::Mesh>(); }
+		_mesh->load(filepath);
+	}
+
+	void SceneView::render() {
+		_shader->use();
+		_light->update(_shader.get());
+		_frameBuffer->bind();
+
+		if (_mesh) {
+			_mesh->update(_shader.get());
+			_mesh->render();
+		}
+
+		_frameBuffer->unbind();
+		ImGui::Begin("Simulation");
+
+		ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
+		_size = { viewportPanelSize.x, viewportPanelSize.y };
+
+		_camera->setAspect(_size.x / _size.y);
+		_camera->update(_shader.get());
+
+		uint64_t textureID = _frameBuffer->getTexture();
+		ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{ _size.x, _size.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
+
+		ImGui::End();
+	}
+}
--- SimulatorOpenGL/Source/UI/SceneView.h
+++ SimulatorOpenGL/Source/UI/SceneView.h
@@ -12,42 +12,42 @@ namespace gui {
     {
     public:
         SceneView() :
-            _Camera(nullptr), _FrameBuffer(nullptr), _Shader(nullptr),
-            _Light(nullptr), _Size(800, 600)
+            _camera(nullptr), _frameBuffer(nullptr), _shader(nullptr),
+            _light(nullptr), _size(800, 600)
         {
-            _FrameBuffer = std::make_unique<render::OpenGLFrameBuffer>();
-            _FrameBuffer->createBuffers(1280, 720);
-            _Shader = std::make_unique<shaders::Shader>();
-            _Shader->load("shaders/vs.shader", "shaders/fs_pbr.shader");
-            _Light = std::make_unique<elements::Light>();
-            _Camera = std::make_unique<elements::Camera>(glm::vec3(0, 0, 3), 45.0f, 1.3f, 0.1f, 100.0f);
+            _frameBuffer = std::make_unique<render::OpenGLFrameBuffer>();
+            _frameBuffer->createBuffers(1280, 720);
+            _shader = std::make_unique<shaders::Shader>();
+            _shader->load("shaders/vs.shader", "shaders/fs_pbr.shader");
+            _light = std::make_unique<elements::Light>();
+            _camera = std::make_unique<elements::Camera>(glm::vec3(0, 0, 3), 45.0f, 1.3f, 0.1f, 100.0f);
 
         }
 
         ~SceneView()
         {
-            _Shader->unload();
+            _shader->unload();
         }
 
-        elements::Light* get_light() { return _Light.get(); }
+        elements::Light* get_light() { return _light.get(); }
 
         void resize(int32_t width, int32_t height);
         void render();
         void loadMesh(const std::string& filepath);
-        void setMesh(std::shared_ptr<elements::Mesh> mesh) { _Mesh = mesh; }
+        void setMesh(std::shared_ptr<elements::Mesh> mesh) { _mesh = mesh; }
 
-        std::shared_ptr<elements::Mesh> get_mesh() { return _Mesh; }
+        std::shared_ptr<elements::Mesh> getMesh() { return _mesh; }
 
         void onMouseMove(double x, double y, elements::eInputButton button);
         void onMouseWheel(double delta);
-        void resetView() { _Camera->reset(); }
+        void resetView() { _camera->reset(); }
 
     private:
-        std::unique_ptr<elements::Camera> _Camera;
-        std::unique_ptr<render::OpenGLFrameBuffer> _FrameBuffer;
-        std::unique_ptr<shaders::Shader> _Shader;
-        std::unique_ptr<elements::Light> _Light;
-        std::shared_ptr<elements::Mesh> _Mesh;
-        glm::vec2 _Size;
+        std::unique_ptr<elements::Camera> _camera;
+        std::unique_ptr<render::OpenGLFrameBuffer> _frameBuffer;
+        std::unique_ptr<shaders::Shader> _shader;
+        std::unique_ptr<elements::Light> _light;
+        std::shared_ptr<elements::Mesh> _mesh;
+        glm::vec2 _size;
     };
 }
