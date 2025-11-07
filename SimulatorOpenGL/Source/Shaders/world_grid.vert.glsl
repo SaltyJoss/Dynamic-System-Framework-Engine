d31d2ffaff278498d6b170b0d9ead33c762bd39f
saltyjoss <joss.salton@gmail.com>
2025-11-07 18:47:18 +0000

    feat: World Grid now ouputs as an a basic interpolation

--- SimulatorOpenGL/Source/Shaders/world_grid.vert.glsl
+++ SimulatorOpenGL/Source/Shaders/world_grid.vert.glsl
@@ -1,6 +1,6 @@
 #version 330 core
 
-out vec3 WorldPos
+out vec3 WorldPos;
 
 uniform mat4 gVP = mat4(1.0);
 uniform vec3 gCameraWorldPos;
@@ -12,11 +12,11 @@ const vec3 Pos[4] = vec3[4](
 	vec3(-1.0, 0.0,  1.0)
 );
 
-const int Indicies[6] = int[6](0, 2, 1, 2, 0, 3);
+const int Indices[6] = int[6](0, 2, 1, 2, 0, 3);
 
 void main()
 {
-	int Index = Indicies[gl_VertexID];
+	int Index = Indices[gl_VertexID];
 	vec3 vPos3 = Pos[Index];
 
 	vPos3.x += gCameraWorldPos.x;
--- SimulatorOpenGL/Source/UI/SceneView.cpp
+++ SimulatorOpenGL/Source/UI/SceneView.cpp
@@ -7,7 +7,10 @@ namespace gui{
 	void SceneView::render() {
 		_frameBuffer->bind();
 
+		glClearColor(1, 1, 1, 1);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 		_worldGridShader->use();
 
