c2d9e8a24e28e6e11af57a3c61f51376ee03a3ce
saltyjoss <joss.salton@gmail.com>
2026-02-20 11:08:46 +0000

    fixes

--- Sim_Engine/EngineLib/include/Platform/imguiWidgets.h
+++ Sim_Engine/EngineLib/include/Platform/imguiWidgets.h
@@ -194,7 +194,7 @@ namespace ImGui {
 
 		// Limits for k to prevent unreasonable values
 		const int MIN_K = 1;
-		const int MAX_K = isTelem ? 10 : 200;
+		const int MAX_K = isTelem ? 10 : 800;
 
 		// Format denominator text
 		char denomBuf[32];
