6f3f83eda41db3e4edd02decf87501a5d1c83463
saltyjoss <joss.salton@gmail.com>
2026-05-04 11:16:48 +0100

    chore: Removed glm includes from DSFE_Core pch.h

--- DSFE_App/DSFE_Core/pch.h
+++ DSFE_App/DSFE_Core/pch.h
@@ -26,13 +26,6 @@
 #include <chrono>
 #include <regex>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include <core/constants.h>
-
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/transform.hpp>
 
 #endif //PCH_H
