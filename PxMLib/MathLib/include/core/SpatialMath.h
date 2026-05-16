bd90252b1c5191af5b7bd21ced7389b0d3d2991d
saltyjoss <joss.salton@gmail.com>
2026-05-16 10:29:06 +0100

    refactor: Updated `SpatialMath.h` to use new Scalar templates

--- PxMLib/MathLib/include/core/SpatialMath.h
+++ PxMLib/MathLib/include/core/SpatialMath.h
@@ -3,105 +3,137 @@
 
 #include "MathLibAPI.h"
 #include "core/Types.h"
+#include "core/Types_tpl.h"
 
 namespace mathlib {
+	using Vec3 = Vec3_T<double>;
+	using Mat3 = Mat3_T<double>;
 
-	using SpatialMat = Eigen::Matrix<double, 6, 6>;
+	// Template version of spatial vector
+	template<typename Scalar>
+	struct SpatialVec_T {
+		using ScalarT = Scalar;
+		using Vec3S = Vec3_T<Scalar>;
+		using Vec6S = Vec6_T<Scalar>;
 
-	struct SpatialVec {
-		Eigen::Matrix<double, 6, 1> v;
+		using AngularBlock = decltype(std::declval<Vec6S&>().template segment<3>(0));
+		using ConstAngularBlock = decltype(std::declval<const Vec6S&>().template segment<3>(0));
 
-		SpatialVec() {
-			v.setZero();
+		using LinearBlock = decltype(std::declval<Vec6S&>().template segment<3>(3));
+		using ConstLinearBlock = decltype(std::declval<const Vec6S&>().template segment<3>(3));
+
+		Vec6S v;
+		
+		SpatialVec_T() { v.setZero(); }
+
+		SpatialVec_T(const Vec3S& angular, const Vec3S& linear) {
+			v.template segment<3>(0) = angular;
+			v.template segment<3>(3) = linear;
 		}
 
-		SpatialVec(const Vec3& angular, const Vec3& linear) {
-			v.segment<3>(0) = angular;
-			v.segment<3>(3) = linear;
-		}
+		AngularBlock angular() { return v.template segment<3>(0); }
+		ConstAngularBlock angular() const { return v.template segment<3>(0); }
 
-		Vec3 angular() const { return v.segment<3>(0); }
-		Vec3 linear() const { return v.segment<3>(3); }
+		LinearBlock linear() { return v.template segment<3>(3); }
+		ConstLinearBlock linear() const { return v.template segment<3>(3); }
 
-		SpatialVec operator+(const SpatialVec& rhs) const {
-			SpatialVec out;
+		SpatialVec_T operator+(const SpatialVec_T& rhs) const {
+			SpatialVec_T out;
 			out.v = this->v + rhs.v;
 			return out;
 		}
 
-		SpatialVec operator-(const SpatialVec& rhs) const {
-			SpatialVec out;
+		SpatialVec_T operator-(const SpatialVec_T& rhs) const {
+			SpatialVec_T out;
 			out.v = this->v - rhs.v;
 			return out;
 		}
 
-		SpatialVec operator*(double rhs) const {
-			SpatialVec out;
+		SpatialVec_T operator*(Scalar rhs) const {
+			SpatialVec_T out;
 			out.v = this->v * rhs;
 			return out;
 		}
 
-		SpatialVec& operator+=(const SpatialVec& rhs) {
+		SpatialVec_T& operator+=(const SpatialVec_T& rhs) {
 			this->v += rhs.v;
 			return *this;
 		}
 
-		SpatialVec operator*=(double rhs) {
+		SpatialVec_T& operator*=(Scalar rhs) {
 			this->v *= rhs;
 			return *this;
 		}
 
-		double dot(const SpatialVec& sv) const {
+		Scalar dot(const SpatialVec_T& sv) const {
 			return this->v.dot(sv.v);
 		}
 	};
+	using SpatialVec = SpatialVec_T<double>;
 
-	inline Mat3 skewSymmetric(const Vec3& v) {
-		Mat3 S;
+	// Template version of spatial matrix
+	template<typename Scalar>
+	using SpatialMat_T = Mat6_T<Scalar>;
+	using SpatialMat = SpatialMat_T<double>;
+
+	template<typename Scalar>
+	inline Mat3_T<Scalar> skewSymmetric(const Vec3_T<Scalar>& v) {
+		Mat3_T<Scalar> S;
 		S <<
-			0, -v.z(), v.y(),
-			v.z(), 0, -v.x(),
-			-v.y(), v.x(), 0;
+			Scalar(0), -v.z(), v.y(),
+			v.z(), Scalar(0), -v.x(),
+			-v.y(), v.x(), Scalar(0);
 
 		return S;
 	}
 
-	inline SpatialMat motionCrossMatrix(const SpatialVec& sv) {
-		SpatialMat X = SpatialMat::Zero();
+	template<typename Scalar>
+	inline SpatialMat_T<Scalar> motionCrossMatrix(const SpatialVec_T<Scalar>& sv) {
+		SpatialMat_T<Scalar> X = SpatialMat_T<Scalar>::Zero();
 
-		Vec3 w = sv.angular();
-		Vec3 v = sv.linear();
+		Vec3_T<Scalar> w = sv.angular();
+		Vec3_T<Scalar> v = sv.linear();
 
-		X.block<3, 3>(0, 0) = skewSymmetric(w);
-		X.block<3, 3>(3, 0) = skewSymmetric(v);
-		X.block<3, 3>(3, 3) = skewSymmetric(w);
+		X.template block<3, 3>(0, 0) = skewSymmetric(w);
+		X.template block<3, 3>(3, 0) = skewSymmetric(v);
+		X.template block<3, 3>(3, 3) = skewSymmetric(w);
 
 		return X;
 	}
 
-	inline SpatialMat forceCrossMatrix(const SpatialVec& sv) {
+	template<typename Scalar>
+	inline SpatialMat_T<Scalar> forceCrossMatrix(const SpatialVec_T<Scalar>& sv) {
 		return (- motionCrossMatrix(sv).transpose()).eval();
 	}
 
-	inline SpatialMat spatialTransform(const Mat3& R, const Vec3& r) {
-		SpatialMat X = SpatialMat::Zero();
+	template<typename Scalar>
+	inline SpatialMat_T<Scalar> spatialTransform(
+		const Mat3_T<Scalar>& R,
+		const Vec3_T<Scalar>& r
+	) {
+		SpatialMat_T<Scalar> X = SpatialMat_T<Scalar>::Zero();
 
-		X.block<3, 3>(0, 0) = R;
-		X.block<3, 3>(3, 0) = -R * skewSymmetric(r);
-		X.block<3, 3>(3, 3) = R;
+		X.template block<3, 3>(0, 0) = R;
+		X.template block<3, 3>(3, 0) = -R * skewSymmetric(r);
+		X.template block<3, 3>(3, 3) = R;
 
 		return X;
 	}
 
-	inline SpatialMat spatialInertia(const double mass, const Vec3& com, const Mat3& I_com) {
-		SpatialMat I = SpatialMat::Zero();
+	template<typename Scalar>
+	inline SpatialMat_T<Scalar> spatialInertia(
+		const Scalar mass,
+		const Vec3_T<Scalar>& com,
+		const Mat3_T<Scalar>& I_com
+	) {
+		SpatialMat_T<Scalar> I = SpatialMat_T<Scalar>::Zero();
 		
-		Mat3 c_skew = skewSymmetric(com);
+		Mat3_T<Scalar> c_skew = skewSymmetric(com);
 
-		I.block<3, 3>(0, 0) = I_com + mass * c_skew.transpose() * c_skew;
-		I.block<3, 3>(0, 3) = mass * c_skew.transpose();
-		I.block<3, 3>(3, 0) = mass * c_skew;
-		I.block<3, 3>(3, 3) = mass * Mat3::Identity();
+		I.template block<3, 3>(0, 0) = I_com + mass * c_skew.transpose() * c_skew;
+		I.template block<3, 3>(0, 3) = mass * c_skew.transpose();
+		I.template block<3, 3>(3, 0) = mass * c_skew;
+		I.template block<3, 3>(3, 3) = mass * Mat3_T<Scalar>::Identity();
 
 		return I;
 	}
@@ -109,55 +141,64 @@ namespace mathlib {
 	// Operator overloads for spatial vector and matrix operations
 
 	// SpatialMat * SpatialVec
-	inline SpatialVec operator*(
-		const SpatialMat& lhs,
-		const SpatialVec& rhs
+	template<typename Scalar>
+	inline SpatialVec_T<Scalar> operator*(
+		const SpatialMat_T<Scalar>& lhs,
+		const SpatialVec_T<Scalar>& rhs
 		) {
-		SpatialVec out;
+		SpatialVec_T<Scalar> out;
 		out.v = lhs * rhs.v;
 		return out;
 	}
 
 	// SpatialVec1 * SpatialVec2 (outer product)
-	inline SpatialMat outer(
-		const SpatialVec& rhs,
-		const SpatialVec& lhs
+	template<typename Scalar>
+	inline SpatialMat_T<Scalar> outer(
+		const SpatialVec_T<Scalar>& rhs,
+		const SpatialVec_T<Scalar>& lhs
 	) {
 		return rhs.v * lhs.v.transpose();
 	}
+
 	// SpatialVec^2 (outer product)
-	inline SpatialMat outer(const SpatialVec& sv) {
+	template<typename Scalar>
+	inline SpatialMat_T<Scalar> outer(const SpatialVec_T<Scalar>& sv) {
 		return sv.v * sv.v.transpose();
 	}
 
 	// SpatialVec1 . SpatialVec2
-	inline double dot(
-		const SpatialVec& lhs,
-		const SpatialVec& rhs
+	template<typename Scalar>
+	inline Scalar dot(
+		const SpatialVec_T<Scalar>& lhs,
+		const SpatialVec_T<Scalar>& rhs
 	) {
 		return lhs.v.dot(rhs.v);
 	}
+
 	// SpatialVec .^2 (element-wise square)
-	inline double dot(const SpatialVec& sv) {
+	template<typename Scalar>
+	inline Scalar dot(const SpatialVec_T<Scalar>& sv) {
 		return sv.v.dot(sv.v);
 	}
 
 	// SpatialVec x SpatialVec (motion cross product)
-	inline SpatialVec crossMotion(
-		const SpatialVec& lhs,
-		const SpatialVec& rhs
+	template<typename Scalar>
+	inline SpatialVec_T<Scalar> crossMotion(
+		const SpatialVec_T<Scalar>& lhs,
+		const SpatialVec_T<Scalar>& rhs
 	) {
-		SpatialVec out;
+		SpatialVec_T<Scalar> out;
 		out.v = motionCrossMatrix(lhs) * rhs.v;
 		return out;
 	}
 
 	// SpatialVec x SpatialVec (force cross product)
-	inline SpatialVec crossForce(
-		const SpatialVec& lhs,
-		const SpatialVec& rhs
+	template<typename Scalar>
+	inline SpatialVec_T<Scalar> crossForce(
+		const SpatialVec_T<Scalar>& lhs,
+		const SpatialVec_T<Scalar>& rhs
 	) {
-		SpatialVec out;
+		SpatialVec_T<Scalar> out;
 		out.v = forceCrossMatrix(lhs) * rhs.v;
 		return out;
 	}
--- PxMLib/MathLib/include/core/Types_tpl.h
+++ PxMLib/MathLib/include/core/Types_tpl.h
@@ -1,22 +1,31 @@
 // PxM/MathLib Types_tpl.h
 #pragma once
 
+#include "MathLibAPI.h"
 #include <Eigen/Dense>
 
-template<typename Scalar>
-using Vec3_T = Eigen::Matrix<Scalar, 3, 1>;
+namespace mathlib {
+	// Template version of 3D vector
+	template<typename Scalar>
+	using Vec3_T = Eigen::Matrix<Scalar, 3, 1>;
 
-template<typename Scalar>
-using Mat3_T = Eigen::Matrix<Scalar, 3, 3>;
+	// Template version of 3x3 matrix
+	template<typename Scalar>
+	using Mat3_T = Eigen::Matrix<Scalar, 3, 3>;
 
-template<typename Scalar>
-using Vec6_T = Eigen::Matrix<Scalar, 6, 1>;
+	// Template version of 6D vector and 6x6 matrix for spatial algebra
+	template<typename Scalar>
+	using Vec6_T = Eigen::Matrix<Scalar, 6, 1>;
 
-template<typename Scalar>
-using Mat6_T = Eigen::Matrix<Scalar, 6, 6>;
+	// Template version of 6x6 matrix for spatial algebra
+	template<typename Scalar>
+	using Mat6_T = Eigen::Matrix<Scalar, 6, 6>;
 
-template<typename Scalar>
-using VecX_T = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;
+	// Template version of dynamic-size vector and matrix
+	template<typename Scalar>
+	using VecX_T = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;
 
-template<typename Scalar>
-using MatX_T = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>;
+	// Template version of dynamic-size matrix
+	template<typename Scalar>
+	using MatX_T = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>;
+}
