4e871c0a22f222275332db6e44f51bd6a98b4191
saltyjoss <joss.salton@gmail.com>
2026-05-17 18:09:42 +0100

    feat: Added implicit integrator tests placeholder, will fill once moved tests over to AD

---
+++ PxMLib/MathLib_Unit/AutomaticDifferentiationTests/ADImplicitIntegratorTests.cpp
@@ -0,0 +1,15 @@
+// MathLib_UnitTests ADImplicitIntegratorTests.cpp
+
+#include "TestHarness.h"
+#include <core/DualNumbers.h>
+#include <integrators/numerical_integrators.h>
+#include <cmath>
+#include <functional>
+#include <core/constants.h>
+
+using namespace mathlib;
+using namespace constants;
+
+namespace {
+	// Will test once implicit methods have been moved to their AD versions
+}
