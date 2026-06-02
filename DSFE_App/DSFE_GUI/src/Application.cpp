45120c39249f67098e56f989f4bce81ee95b4ae7
saltyjoss <joss.salton@gmail.com>
2026-06-02 15:38:09 +0100

    feat: Added basic debug test initialisation of qt6 in `Application`

--- DSFE_App/DSFE_GUI/src/Application.cpp
+++ DSFE_App/DSFE_GUI/src/Application.cpp
@@ -13,9 +13,16 @@
 #include "Platform/Paths.h"
 #include "EngineLib/LogMacros.h"
 
+#include <QCoreApplication>
+#include <qapplication.h>
+#include <qmainwindow.h>
+
 namespace fs = std::filesystem;
 // Initialise the static instance pointer to nullptr
 Application* Application::sInstance = nullptr;
+
+static QApplication* gQtApp = nullptr;
+static QMainWindow* gQtWindow = nullptr;
 
 // Constructor: Initialises paths, sets up data manager, and creates the main application window
 Application::Application(const std::string& appName) {
@@ -28,6 +35,7 @@ Application::Application(const std::string& appName) {
 	LOG_INFO("Runs path: %s", paths::runs().string().c_str());
 
 	int winW = 1920, winH = 1080;
+
 	if (glfwInit()) {
 		const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 		if (mode) {
@@ -43,6 +51,18 @@ Application::Application(const std::string& appName) {
 		glfwTerminate(); // OpenGLContext::init will call glfwInit again
 	}
 
+	if (!gQtApp) {
+		QCoreApplication::addLibraryPath("C:/Qt/6.11.1/msvc2022_64/plugins");
+		qputenv("QT_DEBUG_PLUGINS", "1");
+		int argc = 0;
+		gQtApp = new QApplication(argc, nullptr);
+		gQtWindow = new QMainWindow();
+		gQtWindow->setWindowTitle(QString::fromStdString(appName));
+		gQtWindow->resize(800, 600);
+		gQtWindow->show();
+		LOG_INFO("Qt application and main window created with title '%s'", appName.c_str());
+	}
+
 	_window = std::make_unique<window::GLWindow>();
 	_window->init(winW, winH, appName);
 }
@@ -52,6 +72,8 @@ Application::~Application() = default;
 // Main application loop: Continues running until the window signals to close, updating and rendering each frame
 void Application::run() {
     while (_window->isRunning() && !_window->shouldClose()) {
+		QCoreApplication::processEvents();
+
         _window->update();
         _window->render();
     }
