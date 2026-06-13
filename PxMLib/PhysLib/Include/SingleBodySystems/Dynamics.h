03ea214e44758c7ed5c8dc0be974be696d45a103
saltyjoss <joss.salton@gmail.com>
2026-06-13 13:18:48 +0100

    feat: Added basic `SingleBodyDynamics` class with methods to `computeDynamics` and `computeDerivatives`
     * This is insanely rushed, and I have not checked my math, I want to basically see if I can get the object recognised first, then worry about physical correctness after
     * I doubt ill need to compute the derivatives to begin with but we shall see.

---
+++ PxMLib/PhysLib/Include/SingleBodySystems/Dynamics.h
@@ -0,0 +1,36 @@
+// PxM/PhysLib SingleBodySystems/Dynamics.h
+#pragma once
+#include "SingleBodySystem.h"
+
+namespace single_body_system::dynamics {
+	class SingleBodyDynamics {
+	public:
+		void computeDynamics(SingleBodySystem& body, mathlib::Vec3& externalForce, mathlib::Vec3& externalTorque, double dt) {
+			// Compute linear acceleration
+			body.state.xdd = (externalForce / body.inertia.mass).eval();
+			// Compute angular acceleration
+			mathlib::Mat3 inertiaInv = body.inertia.inertiaTensor.inverse();
+			body.state.wd = inertiaInv * (externalTorque - body.state.w.cross(body.inertia.inertiaTensor * body.state.w));
+			// Update linear velocity and position
+			body.state.xd += body.state.xdd * dt;
+			body.state.x += body.state.xd * dt;
+			// Update angular velocity and orientation (using quaternion integration)
+			body.state.w += body.state.wd * dt;
+			mathlib::Quat dq = mathlib::Quat(0.0, body.state.w * dt);
+			body.state.q = (body.state.q.coeffs() + (0.5 * dq.coeffs() * body.state.q.coeffs())).normalized();
+		}
+
+		void derivatives(const SingleBodySystem& body, mathlib::Vec3& externalForce, mathlib::Vec3& externalTorque, mathlib::Mat3& dxdot_dx, mathlib::Mat3& dxdot_dxdot) {
+			// Compute the Jacobian of the dynamics with respect to state variables
+			dxdot_dx.setZero();
+			dxdot_dxdot.setZero();
+			// Linear acceleration derivatives
+			dxdot_dx.block<3, 3>(0, 0) = mathlib::Mat3::Zero(); // dx/dx = 0
+			dxdot_dxdot.block<3, 3>(0, 3) = mathlib::Mat3::Identity() / body.inertia.mass; // dx/dxdot = 1/mass
+			// Angular acceleration derivatives
+			mathlib::Mat3 inertiaInv = body.inertia.inertiaTensor.inverse();
+			dxdot_dx.block<3, 3>(3, 6) = -inertiaInv * skewSymmetric(body.state.w) * body.inertia.inertiaTensor; // dw/dq
+			dxdot_dxdot.block<3, 3>(3, 9) = inertiaInv; // dw/dwd = I^-1
+		}
+	};
+}
