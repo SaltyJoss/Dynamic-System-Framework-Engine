3b63c49e5ff2dbc2b57792a6675f060a0e2bd8c1
saltyjoss <joss.salton@gmail.com>
2026-06-16 19:00:14 +0100

    fixes: Updated Win32 specific methods to include compile time conditional switch

--- DSFE_App/DSFE_Core/include/Platform/Logger.h
+++ DSFE_App/DSFE_Core/include/Platform/Logger.h
@@ -160,7 +160,11 @@ private:
 		auto now = std::chrono::system_clock::now();
 		std::time_t now_time = std::chrono::system_clock::to_time_t(now);
 		std::tm tm_data;
+	#ifdef _WIN32
 		localtime_s(&tm_data, &now_time);
+	#else
+		localtime_r(&now_time, &tm_data);
+	#endif
 		std::ostringstream oss;
 
 		oss << "[" << std::put_time(&tm_data, "%Y-%m-%d %H:%M:%S") << "] "
