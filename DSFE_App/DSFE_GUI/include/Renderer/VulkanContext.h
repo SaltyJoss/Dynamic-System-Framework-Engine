0fd603e28bd1366698471180470b285704796bba
saltyjoss <joss.salton@gmail.com>
2026-07-18 08:45:13 +0100

    feat: Added VMA memor allocator inlcudes

--- DSFE_App/DSFE_GUI/include/Renderer/VulkanContext.h
+++ DSFE_App/DSFE_GUI/include/Renderer/VulkanContext.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <volk.h>
+#define VMA_STATIC_VULKAN_FUNCTIONS 0
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
+#define VMA_IMPLEMENTATION
+#include <vk_mem_alloc.h>
 
 #include <string>
 #include <vector>
