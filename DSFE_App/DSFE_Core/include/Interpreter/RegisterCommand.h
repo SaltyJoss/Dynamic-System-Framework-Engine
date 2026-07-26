8015070639d01e5ec3442bb32e7fad2f0d34c3f5
saltyjoss <joss.salton@gmail.com>
2026-07-26 15:37:56 +0100

    refactor: Removed old Interpreter (DSL) file names

--- DSFE_App/DSFE_Core/include/Interpreter/RegisterCommand.h
+++
@@ -1,9 +0,0 @@
-// DSFE_Core RegisterCommand.h
-#pragma once
-
-#include "CommandFactory.h"
-
-namespace commands {
-	// Free function to register all commands
-	void RegisterAllCommands(CommandFactory& factory);
-} // namespace commands
