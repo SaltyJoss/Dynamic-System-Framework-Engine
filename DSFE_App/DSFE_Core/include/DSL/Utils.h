fb35214fce7e9a58ffc780cb87312cbf8c224557
saltyjoss <joss.salton@gmail.com>
2026-08-01 09:13:42 +0100

    feat: Added `parseSpatialMask` function to enhance spatial mask interpretation

--- DSFE_App/DSFE_Core/include/DSL/Utils.h
+++ DSFE_App/DSFE_Core/include/DSL/Utils.h
@@ -60,6 +60,7 @@ namespace utils {
 	float parseFloat(const std::string s);
 	mathlib::Vec3 parseVec3(const std::string& str);
 	AxisMask parseAxisMask(const std::string& s);
+	std::vector<AxisMask> parseSpatialMask(const std::string& s);
 	//bool tryParseObjID(const std::string& s, scene::ObjectID& out);
 
 	// --- Unit Conversion Utilities ---
