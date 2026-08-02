a189df83e298dbe4dd6fa986b48e081e7b05bc70
saltyjoss <joss.salton@gmail.com>
2026-08-02 17:52:51 +0100

    refactor: Removed SelectCmd and its related registrations from the command factory

--- DSFE_App/DSFE_Core/src/DSL/RegisterCommand.cpp
+++ DSFE_App/DSFE_Core/src/DSL/RegisterCommand.cpp
@@ -7,21 +7,14 @@
 #include "DSL/RegisterCommand.h"
 
 // Motion commands
-#include "DSL/Commands/SpinCmd.h"
-#include "DSL/Commands/RotateToCmd.h"
-#include "DSL/Commands/RotateByCmd.h"
-#include "DSL/Commands/RotateJointToCmd.h"
-#include "DSL/Commands/RotateJointByCmd.h"
 #include "DSL/Commands/TrajSetCmd.h"
 #include "DSL/Commands/TrajClearCmd.h"
-#include "DSL/Commands/SetOmegaCmd.h"
 #include "DSL/Commands/SetVelocityCmd.h"
 
 // Primary function commands
 #include "DSL/Commands/StartCmd.h"
 #include "DSL/Commands/StopCmd.h"
 #include "DSL/Commands/WaitCmd.h"
-#include "DSL/Commands/SelectCmd.h"
 #include "DSL/Commands/LoadCmd.h"
 #include "DSL/Commands/SetCmd.h"
 
@@ -29,20 +22,13 @@ namespace commands {
 	// Register all commands with the factory
 	void RegisterAllCommands(CommandFactory& factory) {
 		// Motion commands
-		factory.registerCommand("spin",				&commands::CreateSpinCmd);			// spin command
-		factory.registerCommand("rotateto",			&commands::CreateRotateToCmd);		// rotate command
-		factory.registerCommand("rotateby",			&commands::CreateRotateByCmd);		// rotate command
-		factory.registerCommand("rotatejointto",	&commands::CreateRotateJointToCmd);	// rotateJoint command
-		factory.registerCommand("rotatejointby",	&commands::CreateRotateJointByCmd);	// rotateJoint command
 		factory.registerCommand("trajset",			&commands::CreateTrajSetCmd);		// trajSet command
 		factory.registerCommand("trajclear",		&commands::CreateTrajClearCmd);		// trajClear command
-		factory.registerCommand("setomega",			&commands::CreateSetOmegaCmd);		// setOmega command
 		factory.registerCommand("setvelocity", 		&commands::CreateSetVelocityCmd);	// setVelocity command
 		// Primary Function commands
 		factory.registerCommand("start",			&commands::CreateStartCmd);	 // start command
 		factory.registerCommand("stop",				&commands::CreateStopCmd);	 // stop command
 		factory.registerCommand("wait",				&commands::CreateWaitCmd);	 // pause command
-		factory.registerCommand("select",			&commands::CreateSelectCmd); // select command
 		factory.registerCommand("load",				&commands::CreateLoadCmd);	 // load command
 		factory.registerCommand("set",				&commands::CreateSetCmd);	 // set command
 		// New commands later
